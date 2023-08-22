2dd576e9ca748707d7e2f0dbf2160114b1f0a982
lavrickNG <lavrickboyi18@gmail.com>
2023-08-22 05:38:47 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar(digit + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
