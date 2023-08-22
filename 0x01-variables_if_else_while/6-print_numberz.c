93e0f1bd53e2863ba4c9040f25e32d1722bd1263
lavrickNG <lavrickboyi18@gmail.com>
2023-08-22 05:57:17 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
 /**
+ * print_digit - Prints a single digit character.
+ * @digit: The digit to be printed.
+ */
+void print_digit(int digit)
+{
+	putchar(digit + '0');
+}
+
+/**
  * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	int i;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		print_digit(i);
+	}
+
 	putchar('\n');
 
 	return (0);
